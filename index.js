082b541df36e7d74aae88e85e4623413fa329df8
Anushikha12 <100060666+Anushikha12@users.noreply.github.com>
2022-08-25 16:46:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//1.)what is higher order components?
+
+//--Higher order components are advanced technique for reusing component logic.
+//--Basically,its a function that takes a component and returns a new component.
+//it is easy to handle
+//gets rid of copying the same logic in every component.
+//it makes our code more readable.
+
+//2.)What is pure component?
+
+//Pure Component Class compares current state and props with new props and states to decide whether the React component should re-render itself or not.
+//PureComponent class are treated as pure components.
+//A React component is considered pure if it renders the same output for the same state and props.
